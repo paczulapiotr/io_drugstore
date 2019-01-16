b1c7272f4a8c54498f9dff40380ad683a04e05b5
paczulapiotr <paczulapiotr@gmail.com>
2019-01-16 22:08:00 +0100

    Finished gulp scripts

--- Drugstore/gulpfile.js
+++ Drugstore/gulpfile.js
@@ -1,12 +1,20 @@
-/// <binding BeforeBuild='sass, bundle, minify-css' />
+/// <binding BeforeBuild='clean-css, sass, bundle, minify-css' ProjectOpened='sass:watch' />
 'use strict';
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concatCss = require('gulp-concat-css');
+var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
+var clean = require('gulp-clean');
 
 sass.compiler = require('node-sass');
+
+
+gulp.task('clean-css', function () {
+    return gulp.src('./wwwroot/styles/**/*.css', { read: false })
+        .pipe(clean());
+});
 
 gulp.task('sass', function () {
   return gulp.src('./wwwroot/styles/scss/**/*.scss')
@@ -14,10 +22,6 @@ gulp.task('sass', function () {
       .pipe(gulp.dest('./wwwroot/styles/css'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./wwwroot/styles/*/*.scss', ['sass']);
-});
-
 gulp.task('bundle', function () {
     return gulp.src('./wwwroot/styles/css/**/*.css')
         .pipe(concatCss("styles/bundle.css"))
@@ -25,7 +29,12 @@ gulp.task('bundle', function () {
 });
 
 gulp.task('minify-css', () => {
-  return gulp.src('./wwwroot/styles/css/bundle.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./wwwroot/styles/css/'));
+  return gulp.src('./wwwroot/styles/bundle.css')
+	.pipe(cleanCSS({compatibility: 'ie8'}))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('./wwwroot/styles'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./wwwroot/styles/scss/**/*.scss', gulp.series(['sass', 'bundle', 'minify-css']));
 });
