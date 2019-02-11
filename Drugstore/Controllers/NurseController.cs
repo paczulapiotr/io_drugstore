af807c8cc7d0ca9d2f63e768d879294976666f2d
Marek Żabiałowicz <marekzabialowicz@gmail.com>
2019-02-11 09:32:02 +0100

    Add NurseController

---
+++ Drugstore/Controllers/NurseController.cs
@@ -0,0 +1,33 @@
+﻿using Drugstore.Identity;
+using Drugstore.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Drugstore.Controllers
+{
+    [Authorize(Roles = "Nurse")]
+    public class NurseController : Controller
+    {
+        private readonly DrugstoreDbContext context;
+        private readonly UserManager<SystemUser> userManager;
+
+        public NurseController(DrugstoreDbContext context, UserManager<SystemUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Circiut()
+        {
+            return Ok();
+        }
+    }
+}
